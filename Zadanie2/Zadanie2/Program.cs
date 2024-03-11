e6203c035e6a56e62ac35997ecd8ddc03cc69963
Yurii Lishchuk <s28225@pjwstk.edu.pl>
2024-03-11 16:27:34 +0100

    Modyfikacja 1

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -4,12 +4,6 @@
     {
         int[] tab = { 1, 2, 3, 4 };
         Console.WriteLine(tab.Average());
-
-        foreach (int num in tab)
-        {
-            Console.WriteLine(num);
-        }
         
-        Console.WriteLine("The last modification");
     }
 }
