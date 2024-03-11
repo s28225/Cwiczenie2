90bd3448c1389eadba510ccdb06e0b04d78852d1
Yurii Lishchuk <s28225@pjwstk.edu.pl>
2024-03-11 16:40:15 +0100

    AVERAGE1

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -3,7 +3,17 @@
     public static void Main(string[] args)
     {
         int[] tab = { 1, 2, 3, 4 };
-        Console.WriteLine(tab.Average());
-        
+
+    }
+
+    public static double MyAverage(int[] tab)
+    {
+        int suma = 0;
+        foreach (int num in tab)
+        {
+            suma += num;
+        }
+
+        return (double)suma / tab.Length;
     }
 }
